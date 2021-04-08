c30011f35159e782af8683e332c2df4d89ab4552
jtuttas <jtuttas@gmx.net>
2021-04-08 16:46:37 +0200

    python

---
+++ scanner.py
@@ -0,0 +1,55 @@
+import cv2
+import requests
+from pyzbar import pyzbar
+
+def read_barcodes(frame):
+    barcodes = pyzbar.decode(frame)
+    for barcode in barcodes:
+        x, y , w, h = barcode.rect
+        
+        barcode_info = barcode.data.decode('utf-8')
+        
+        
+        
+        print("READ:"+barcode_info)
+        txt = str(barcode_info)
+        txt=str(txt.replace("%2B","+"))
+        if txt.index("id=")!=-1:
+            rsa = txt[txt.index("id=")+3:]           
+            print("RSA:"+str(rsa))
+            r = requests.post('https://localhost:8080/validate', json={"id": rsa}, verify=False)
+            data = r.json()
+            if data["valid"]:
+                cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+                font = cv2.FONT_HERSHEY_DUPLEX
+                cv2.putText(frame, data["vn"]+" "+data["nn"], (x + 6, y - 6),
+                        font, 2.0, (255, 255, 255), 1)
+            else:
+                cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+                font = cv2.FONT_HERSHEY_DUPLEX
+                cv2.putText(frame, "Invalid", (x + 6, y - 6),
+                        font, 2.0, (255, 255, 255), 1)
+        else:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, "Invalid", (x + 6, y - 6),
+                        font, 2.0, (255, 255, 255), 1)
+    return frame
+
+def main():
+    #1
+    camera = cv2.VideoCapture(1)
+    ret, frame = camera.read()
+    #2
+    while ret:
+        ret, frame = camera.read()
+        frame = read_barcodes(frame)
+        cv2.imshow('Barcode/QR code reader', frame)
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
+    #3
+    camera.release()
+    cv2.destroyAllWindows()
+#4
+if __name__ == '__main__':
+    main()
