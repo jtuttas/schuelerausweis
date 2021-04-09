a23e63cf6d336e18fc67d184de9f9565a40f132a
jtuttas <jtuttas@gmx.net>
2021-04-09 08:34:02 +0200

    mit richtigem Content-Type

--- scanner.py
+++ scanner.py
@@ -2,38 +2,54 @@ import cv2
 import requests
 from pyzbar import pyzbar
 
+oldbarcode_info=""
+oldText=""
+
 def read_barcodes(frame):
     barcodes = pyzbar.decode(frame)
+    global oldbarcode_info
+    global oldText
     for barcode in barcodes:
         x, y , w, h = barcode.rect
         
-        barcode_info = barcode.data.decode('utf-8')
+        barcode_info = barcode.data.decode('utf-8')        
         
-        
-        
-        print("READ:"+barcode_info)
-        txt = str(""+barcode_info)
-        txt=str(txt.replace("%2B","+"))
-        if txt.find("id=")!=-1:
-            rsa = txt[txt.index("id=")+3:]           
-            print("RSA:"+str(rsa))
-            r = requests.post('https://localhost:8080/validate', json={"id": rsa}, verify=False)
-            data = r.json()
-            if data["valid"]:
-                cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-                font = cv2.FONT_HERSHEY_DUPLEX
-                cv2.putText(frame, data["vn"]+" "+data["nn"], (x + 6, y - 6),
+        if oldbarcode_info==barcode_info:
+            print("Old Code")
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, oldText, (x + 6, y - 6),
                         font, 2.0, (255, 255, 255), 1)
+
+        else:
+            print("READ:"+barcode_info)
+            oldbarcode_info=barcode_info
+            txt = str(""+barcode_info)
+            txt = str(txt.replace("%2B", "+"))
+
+            if txt.find("id=")!=-1:
+                rsa = txt[txt.index("id=")+3:]           
+                print("RSA:"+str(rsa))
+                r = requests.post('https://localhost:8080/validate', json={"id": rsa}, verify=False)
+                data = r.json()
+                if data["valid"]:
+                    cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+                    font = cv2.FONT_HERSHEY_DUPLEX
+                    cv2.putText(frame, data["vn"]+" "+data["nn"], (x + 6, y - 6),
+                            font, 2.0, (255, 255, 255), 1)
+                    oldText = data["vn"]+" "+data["nn"]
+                else:
+                    cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+                    font = cv2.FONT_HERSHEY_DUPLEX
+                    cv2.putText(frame, "Invalid", (x + 6, y - 6),
+                            font, 2.0, (255, 255, 255), 1)
+                    oldText = "Invalid"
             else:
                 cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
                 font = cv2.FONT_HERSHEY_DUPLEX
                 cv2.putText(frame, "Invalid", (x + 6, y - 6),
-                        font, 2.0, (255, 255, 255), 1)
-        else:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-            font = cv2.FONT_HERSHEY_DUPLEX
-            cv2.putText(frame, "Invalid", (x + 6, y - 6),
-                        font, 2.0, (255, 255, 255), 1)
+                            font, 2.0, (255, 255, 255), 1)
+                oldText="Invalid"
     return frame
 
 def main():
